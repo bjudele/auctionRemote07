0a0dbcd51f9b5cba84a02925e56e219d45778586
Bogdan Judele <bogdan.judelle@gmail.com>
2021-02-06 14:19:39 +0200

    Added registration page

--- src/main/java/com/sda/auction/controller/HomeController.java
+++ src/main/java/com/sda/auction/controller/HomeController.java
@@ -1,17 +1,33 @@
 package com.sda.auction.controller;
 
+import com.sda.auction.dto.UserDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String getHomepage(){
-        System.out.println("heeey!!!!!!!!!!!!!!!");
+    public String getHomepage() {
+
         return "home";
     }
 
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        System.out.println("Se apeleaza get register");
+        model.addAttribute("userDto", new UserDto());
+
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String postRegisterPage(UserDto userDto) {
+        System.out.println("Se apeleaza post register cu "+userDto);
+        return "register";
+    }
 
 
 }
---
+++ src/main/java/com/sda/auction/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.sda.auction.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Boolean isAdmin;
+}
